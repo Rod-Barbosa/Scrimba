bca9e985a670b9de530d0c20b137112c903ebc88
Getgud <rodrigopiedadecareer@gmail.com>
2021-11-22 18:19:44 -0500

    Add files via upload

---
+++ Module-06-Next-level-javascript/Challenges/04-Factorial/main.js
@@ -0,0 +1,29 @@
+function factorialNumber(num) {
+    // write code here
+    let sum = 1;
+    for(let i=1;i<=num;i++){
+        sum*=i
+    }
+    return sum
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('factorialNumber()', () => {
+    it('returns factorial of number param', () => {
+        // arrange
+        const num = 5;
+        
+        // act
+        const result = factorialNumber(num);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toBe(120);
+    });
+});
