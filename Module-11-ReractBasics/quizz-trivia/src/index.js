8ae474f4bca2dd3ea925b7cb18899057b6138335
Getgud <rodrigopiedadecareer@gmail.com>
2021-12-25 17:34:35 -0500

    Add files via upload

---
+++ Module-11-ReractBasics/quizz-trivia/src/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import "./style.css"
+
+// figma file: https://www.figma.com/file/wJ4TllIgkqKmEaYBbkl2eJ/Quizzical-App-(Copy)
+
+ReactDOM.render(<App />
+    , document.getElementById("root"))
